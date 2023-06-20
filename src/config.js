8118355bbf6e7c469f8619e7db0d054c8ca9f9b8
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-06-20 11:20:01 +0300

    post delete

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://jsonplaceholder.typicode.com";
+export const API_URL = "http://localhost:3000";
 export const POSTS_PER_PAGE = 20;
 export const ALBUMS_PER_PAGE = 16;
--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -3,10 +3,12 @@ import React from "react";
 import { API_URL } from "../../config";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Post() {
   const { id } = useParams();
   const [post, setPosts] = useState(null);
+  const [postDeleted, setPostDeleted] = useState(false);
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(`${API_URL}/posts/${id}?_expand=user&_embed=comments`);
@@ -22,21 +24,38 @@ function Post() {
     return "";
   }
 
+  const deleteHandler = () => {
+    axios.delete(`${API_URL}/posts/${id}?_expand=user&_embed=comments`).then((data) => setPostDeleted(true));
+  };
+
   return (
     <Container>
-      <h2>{post.title}</h2>
-      <a href={`/json-users/${post.user.id}`}>{post.user.name} </a>
-      <a href={`/json-posts/${post.user.id}`}>Next author post's </a>
-      {/* kazkasneveikia, su ejimu i kitus author posts */}
-      <a href={`/json-posts`}>Back to post's</a>
-      <p>{post.body}</p>
-      {post.comments.map((comment) => (
-        <div key={comment.id}>
-          <h3>{comment.name}</h3>
-          <p>Title: {comment.body}</p>
-          <a href={"mailto:" + comment.email}>{comment.email}</a>
-        </div>
-      ))}
+      {postDeleted ? (
+        <>
+          <h1>Post was deleted</h1>
+          <a href={`/json-posts`}>Back to post's</a>
+        </>
+      ) : (
+        post && (
+          <>
+            <h2>{post.title}</h2>
+            <button onClick={deleteHandler}>Delete post</button>
+            <a href={`/json-users/${post.user.id}`}>{post.user.name} </a>
+            <a href={`/json-posts/${post.user.id}`}>Next author post's </a>
+            {/* kazkasneveikia, su ejimu i kitus author posts */}
+
+            <p>{post.body}</p>
+            {post.comments.map((comment) => (
+              <div key={comment.id}>
+                <h3>{comment.name}</h3>
+
+                <p>Title: {comment.body}</p>
+                <a href={"mailto:" + comment.email}>{comment.email}</a>
+              </div>
+            ))}
+          </>
+        )
+      )}
     </Container>
   );
 }
